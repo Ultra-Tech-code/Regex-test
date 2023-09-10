e25e45f164fa0ea9da4442da593b9398f0979cc8
Ultra-tech-code <hizick27@gmail.com>
2023-09-10 19:10:18 +0100

    first push

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+ let input = prompt("Enter your input: ")
+// let regex = /.*@.*\.com/
+// let result = input.search(regex)
+
+
+// if (result == "-1") {
+//   alert("Incorrect Email");
+// } else {
+//   alert(input.replace(regex, "************"));
+// }
+
+
+//1. car and cat
+
+// regex = /(car)|(cat)/i
+// result = input.match(regex)
+// alert(result)
+
+
+// //2. pop and prop
+
+// // regex = /(pop)|(prop)/
+// // result = input.match(regex)
+// // alert(result)
+
+// //3. ferret, ferry, and ferrari
+// // regex = /\b(ferret)\b|\b(ferry)\b|\b(ferrari) 
+
+// // regex = /(ferret)|(ferry)|(ferrari)/
+// // result = input.match(regex)
+// // alert(result)
+
+// // 4. Any word ending in ious
+// // regex = /\w+(ious)/
+// // result = input.match(regex)
+// // alert(result)
+
+//5. A whitespace character followed by a period, comma, colon, or semicolon
+regex = /\s+\.\,(:|;)/
+result = input.search(regex)
+alert(result)
+
+// 
+
