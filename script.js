b2226a1d55641fd559b4971c9f0db818b655742a
Ultra-tech-code <hizick27@gmail.com>
2023-09-14 21:35:34 +0100

    improvement

--- script.js
+++ script.js
@@ -1,15 +1,22 @@
+//  Task one: replacing mails with *****
+function gmailConveter(e){
+ e.preventDefault;
 
+let input = document.getElementById("textInput").value
+const gmailResult = document.getElementById('gmailResult')
 
- let input = prompt("Enter your input: ")
-// let regex = /.*@.*\.com/
-// let result = input.search(regex)
+let regex = /.*@.*\.com/
+let result = input.search(regex)
 
-
-// if (result == "-1") {
-//   alert("Incorrect Email");
-// } else {
-//   alert(input.replace(regex, "************"));
-// }
+    if (result == "-1") {
+    gmailResult.innerHTML = `<p>Incorrect Email</p>`
+    } else {
+        gmailResult.innerHTML = `<div>
+        <p>Successful</p>
+        <p>${input.replace(regex, "************")}</p>
+        </div>`
+    }
+}
 
 
 //1. car and cat
@@ -49,8 +56,25 @@
 
 //7. A word without the letter e (or E)
 
-regex = /\b[^e]+\b/i
-result = input.search(regex)
-alert(result)
+// regex = /\b[^e]+\b/i
+// result = input.search(regex)
+// alert(result)
+
+
+//8 
+
+// Imagine you have written a story and used single quotation marks throughout
+// to mark pieces of dialogue. Now you want to replace all the dialogue quotes
+// with double quotes, while keeping the single quotes used in contractions like
+// aren’t.
+// Think of a pattern that distinguishes these two kinds of quote usage and
+// craft a call to the replace method that does the proper replacement.
+
+// regex =  /'([^']+)'/g;
+// result = input.replace(regex, '"$1"')
+
+// const replacedText = text.replace(regex, (match, capturedText) => `"${capturedText}"`);
+// alert(result)
+
 
 
